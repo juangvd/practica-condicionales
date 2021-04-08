5118213c25601a70ef86b4f058353fe3e48b86e1
juangvd <77943731+juangvd@users.noreply.github.com>
2021-04-08 17:06:32 -0400

    Add files via upload

---
+++ condicionales.js
@@ -0,0 +1,23 @@
+/*
+var num = 15;
+if (num >= 10) {
+    console.log("el número es mayor que 10");
+}
+*/
+
+/*
+var name = "yo";
+if (name != "yo") {
+    console.log("¿Cuál es tu nombre?");
+} else {
+    console.log("Hola, Juan!");
+}
+*/
+
+var num1 = 10;
+var num2 = 15;
+if (num1 <= num2) {
+    num1 = num1 + 10;
+}
+console.log(num1);
+console.log(num2);
